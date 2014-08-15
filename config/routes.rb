95af2966b0f33880379eb11fa024515fa30d0fea
Madhankumar Nagaraj <n.madhan@yahoo.co.in>
2014-08-15 09:03:38 +0530

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ Rails.application.routes.draw do
   resources :articles do 
     resources :comments
   end
+  
+  resources :tags
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
